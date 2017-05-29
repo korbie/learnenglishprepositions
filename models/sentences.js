db0a264db5406a5b47367555308b3697ec2ca0b2
korbie <mattcorbin@gmail.com>
2017-05-29 22:00:41 +0300

    Basic front end functionality almost done

--- models/sentences.js
+++ models/sentences.js
@@ -1,25 +1,76 @@
 var fs = require('fs');
 
-var sampleSentence = {
-	text : 'The cup is on the table.',
-	on : {start : 11, length : 2, definition: 'on1'},
-	iteration: 0
+var sampleSentence1 = {
+	text : ['The pen is', '#on#1', 'the notebook', '.'],
+	img : '1000'
+};
+var sampleSentence2 = {
+	text : ['The saucer is', '#under#1', 'the cup', '.'],
+	img : '1000'
+};
+var sampleSentence3 = {
+	text : ['The pencils are', '#in#1', 'the cup', '.'],
+	img : '1001'
+};
+var sampleSentence4 = {
+	text : ['The notebook is', '#beside#1', 'the cup', '.'],
+	img : '1001'
 };
 
+var sentences = [sampleSentence1, sampleSentence2, sampleSentence3, sampleSentence4];
+
+var isPunctuation = function(e){
+	return (e === '.' || e === '?' || e === '!' || e === '...');
+};
+
+var findDefinition = function(j){
+	return j;
+};
+
+var parsePreposition = function(e, i){
+	var chars = e.split('');
+	var txt = '';
+	var def = '';
+	var output = {t : '', p : true, d : 'unknown definition'};
+	var i = 1;
+	var foundEnd = false;
+	for (i = 1; i < chars.length; i++){
+		if (chars[i] === '#'){
+			foundEnd = true;
+			continue;
+		}
+		else if (foundEnd){
+			def += chars[i];
+		}
+		else{
+			txt += chars[i];
+		}
+	}
+	output.t = (txt);
+	output.d = findDefinition(parseInt(def));
+	return output;
+};
 
 var getSentence = function(res){
-	fs.readFile(__dirname + '/../data_development/in.txt', 'utf8', function(err, data){
-		var sentences = [];
-		if (err){
-			console.error(err);
+	var sentenceNumber = Math.floor(Math.random() * 4);
+	var output = {	txt : [],
+					fileName: ''};
+	output.fileName = sentences[sentenceNumber].img;
+	sentences[sentenceNumber].text.forEach(function(e, i){
+		if (i !== 0 && e.charAt(0) !== '#' && !isPunctuation(e)){
+			output.txt.push({t : (' ' + e), p : false});
 		}
-		else {
-			console.log('getting sentence...');
-			sentences = data.split('\n');
-			sampleSentence.iteration ++;
-			res.json(sampleSentence);
+		else if ((i === 0 || isPunctuation(e)) && e.charAt(0) !== '#'){
+			output.txt.push({t : e, p : false});
 		}
-	}); 
+		else if (e.charAt(0) === '#'){
+			output.txt.push(parsePreposition(e, i));
+			if (i !== 0){//add space to end of prior string
+				output.txt[i - 1].t = (output.txt[i - 1].t += ' ')
+			}
+		}
+	});
+	res.json(output);
 };
 
 exports.getSentence = getSentence;
--- public/libs/main.js
+++ public/libs/main.js
@@ -1,13 +1,15 @@
-var currentLink = '';
-
 
 var manager = (function(){
 	var numberOptions = 3;
-	var activePrepositions = ['to','from','in','at','by','on'];
+	var activePrepositions = ['on','under','in','beside'];
+	var currentSentence = {};
 	
-	var toggleActivePrepositions = function(preposition){
+	var toggleActivePrepositions = function(p){
+		var preposition = $(p).html();
 			if (activePrepositions.indexOf(preposition) === -1){
 				activePrepositions.push(preposition);
+				$(p).toggleClass('active');
+				$(p).attr('aria-pressed',reverseAriaPressed(p));
 			}
 			else if (activePrepositions.indexOf(preposition) !== -1){
 				if (activePrepositions.length === 2){
@@ -18,35 +20,83 @@ var manager = (function(){
 						return e !== preposition;
 					});
 					if (numberOptions === activePrepositions.length){
-						numberOptions--;
+						decreaseOptions();
 					}
+					$(p).toggleClass('active');
+					$(p).attr('aria-pressed',reverseAriaPressed(p));
 				}
 			}
 		};
-		
+	
+	var reverseAriaPressed = function(p){
+		var pressed = $(p).attr('aria-pressed');
+		console.log('aria-pressed: ' + pressed);
+		pressed = pressed ? false : true ;
+		return pressed;
+	};
+	
 	var getActivePrepositions = function(){
 			return activePrepositions;
 		};
 		
-	var increaseOptions = function(){
-			if (numberOptions < activePrepositions.length - 1){
-				numberOptions++;
-				return true;
-			}
-			else {
-				return false;
-			}
-		};
+	var shuffleArray = function(array){
+		var currentIndex = array.length,
+		temporaryValue,
+		randomIndex;
+
+		while (currentIndex !== 0) {
+
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	  }
+	  return array;
+	};
+	
+	var getRandomPrepositions = function(currentPreposition){//returns randomized array including correct answer, plus number of random options equal to numberOptions
+		var arr,
+			i,
+			result = [],
+			numberNeeded = numberOptions;
+			
+		arr = shuffleArray(activePrepositions);
 		
-	var decreaseOptions = function(){
-			if (numberOptions > 1){
-				numberOptions--;
-				return true;
+		for (i = 0; i < numberNeeded; i++){
+			if (arr[i] === currentPreposition){
+				numberNeeded++;
+				continue;
 			}
-			else{
-				return false;
-			}
-		};
+			result.push(arr[i]);
+		}
+		result.push(currentPreposition);
+		return shuffleArray(result);
+	};
+		
+	var increaseOptions = function(){//increases options up to upper limit of one fewer than the total number of prepositions 
+		if (numberOptions < activePrepositions.length - 1){
+			numberOptions++;
+			$('#numberOptions').html(numberOptions);
+			return true;
+		}
+		else {
+			return false;
+		}
+	};
+		
+	var decreaseOptions = function(){//decreases options down to the lower limit of 1
+		if (numberOptions > 1){
+			numberOptions--;
+			$('#numberOptions').html(numberOptions);
+			return true;
+		}
+		else{
+			return false;
+		}
+	};
+	
 	var changeNumberOptions = function(instruction){
 		if (instruction === 'increase'){
 			return increaseOptions();
@@ -56,170 +106,202 @@ var manager = (function(){
 		}
 	};
 	
-	var getNumberOptions = function(){
-		return numberOptions;
+	var setOptionButtons = function(){
+		var btns = $("#btnsNumberOptions").children(".btn");
+		btns.each(function(i, e){
+			var p = $(e).attr('name'),
+			f = function(func, arg){
+				return (function(){
+					func(arg);
+				});
+			};
+		$(e).on('click', f(changeNumberOptions,p));		
+		});
 	};
+	
+	var setPrepositionButtons = function(){
+		var btns = $("#btnsPrepositions").children(".btn");
+		btns.each(function(i, e){
+			var p = $(e),
+				f = function(func, arg){
+					return (function(){
+						func(arg);
+					});
+				};
+			$(e).attr('aria-pressed', 'true').addClass('active');
+			$(e).on('click', f(toggleActivePrepositions,p));		
+		});
+	};
+	
+	var setVisualAid = function(fileName){
+		var path = 'img/' + fileName + '.jpg';
+		$('#visualAid').attr('src', path);
+	};
+	
+	var deploySentence = function(data){
+		$('.sentence_container').empty();
+		setVisualAid(data.fileName);
+		answerChecker.newSentenceSetup();
+		var sentence = '';
+		var i;
+		var txt = '';
+		for (i = 0; i < data.txt.length; i++){
+			if (data.txt[i].p && txt.length > 0){//NOTE: TODO address the case where the preposition is the first word in the sentence...			
+				if (activePrepositions.indexOf(data.txt[i].t) === -1){
+					txt += data.txt[i].t;
+					continue;
+				}
+				var obj = makeLink(data.txt[i].t);
+				var lnk = $('<a>').attr({'data-toggle':'modal','data-target':'#optionsModal'}).html(obj.t).on('click', obj.f);
+				answerChecker.newLink(data.txt[i].t, lnk);
+				$('<span>').html(txt).appendTo($('.sentence_container'));
+				$('<span>').append(lnk).appendTo($('.sentence_container'));
+				txt = '';
+			}
+			else if (!data.txt[i].p){
+				txt += data.txt[i].t;//need to add a space??
+			}	
+		}
+		if (txt.length > 0){
+			$('<span>').html(txt).appendTo($('.sentence_container'));
+		}
+		$('#repeatSentence').addClass('disabled');
+		$('#getSentence').addClass('disabled');
+		$('#checkAnswers').removeClass('disabled');
 		
+	};
+	
+	var makeLink = function(p){//should return object containing link html (randomized) and function
+	
+		var result = {t: null, f: null};
+		
+		var currentActive = p;
+		var prepositions = getRandomPrepositions(p);
+		result.t = prepositions.pop();
+		
+		var updatePrepositions = function(p, o){
+			var index = prepositions.indexOf(p);
+			prepositions[index] = o;
+		};
+		
+		var prepositionButtonAction = function(lnk){
+			return (function(){
+				var buttonName = $(this).html();
+				var linkName = lnk.html();
+				lnk.html(buttonName);
+				$(this).html(linkName);
+				updatePrepositions(buttonName, linkName);
+			});
+		};
+		
+		result.f = function(){
+			var i,
+			m = $('#optionsButtons'),
+			action;
+			m.children().remove();
+	
+			for (i = 0; i < numberOptions; i++){
+				action = prepositionButtonAction($(this));
+				m.append($('<button>').attr({'class':'btn btn-default','type':'button','data-toggle':'modal','data-target':'#optionsModal'}).html(prepositions[i]).on('click', action));	
+			}
+		};
+	
+		return result;
+	};
+	
+	var answerChecker = (function(){
+		var links = [];
+		var correctAnswers = [];
+		
+		var newSentenceSetup = function(){
+			links = [];
+			correctAnswers = [];
+		};
+		
+		var newLink = function(a, l){
+			correctAnswers.push(a);
+			links.push(l);
+		}
+		
+		var checkAnswers = function(){
+			for (var i = 0; i < links.length; i++){
+				if ($(links[i]).html() !== correctAnswers[i]){
+					$(links[i]).addClass('incorrect');
+				}
+				else{
+					$(links[i]).addClass('correct');
+				}
+				$(links[i]).removeAttr('data-toggle data-target').prop( "onclick", null);
+				$('#repeatSentence').removeClass('disabled');
+				$('#getSentence').removeClass('disabled');
+				$('#checkAnswers').addClass('disabled');
+			}
+		};
+		
+		return {
+			newSentenceSetup : newSentenceSetup,
+			newLink : newLink,
+			checkAnswers : checkAnswers
+		}
+
+	}());
+	
+	var getNewSentence = function(){
+		$.ajax({
+			method: 'POST',
+			data : {activePrepositions: activePrepositions},//need to return only those sentences that include at least one instance of an active preposition...
+			error:function(err){
+				console.log(err);
+				//return '';
+			},
+			success:function(data){
+				deploySentence(data);
+				$('#repeatSentence').on('click',(function(a, b){return function(){a(b)}}(deploySentence, data)));
+			}
+		});
+	};
+	
 	var output =	{	toggleActivePrepositions: toggleActivePrepositions,
-						getActivePrepositions : getActivePrepositions,
-						increaseOptions : increaseOptions,
 						changeNumberOptions : changeNumberOptions,
-						getNumberOptions : getNumberOptions						
+						getNewSentence : getNewSentence,
+						checkAnswers : answerChecker.checkAnswers,
+						setOptionButtons : setOptionButtons,
+						setPrepositionButtons : setPrepositionButtons
 					};
 	
 	return output;
 }());
 
-
 /*************/
-var getNewSentence = function(){};
-var getActivePrepositions = function(){};//these preferences could be stored on the server side for better persistence.  So maybe don't need this function here?  Or set a flag--if the preferences are changed, then get the new preferences and update them on the server side.  If not updated, then just request a new sentence from server, which will deliver a sentence with reference to the stored preferences
-//server side functions will return a sentence that includes one or more of the active prepositions, and which hasn't been shown lately
 
-var formatNewSentence = function(){};
-var populateModal = function(){};
-var randomizePrepositions = function(){};
-
-var updateSelectedPreposition = function(){};
-var correctSentence = function(){};
-
-
-
-/**************/
-
-
-
-
-var buttonAction = function(){
-	$('#getSentence').on('click', function(){
-		//console.log('clicking...');
-		$.ajax({
-			method: 'POST',
-			error:function(err){
-				console.log(err);
-			},
-			success:function(data){
-				$('.sentence_container').html(data.text + ' ' + data.iteration);
-			}
+var setup = function(){
+	
+	var setModals = function(){
+		$(".modal-wide").on("show.bs.modal", function() {
+			var height = $(window).height() - 200;
+			$(this).find(".modal-body").css("max-height", height);
 		});
-	});
-};
-
-var getNewSentence = function(){
-	$.ajax({
-		method: 'POST',
-		error:function(err){
-			console.log(err);
-			return '';
-		},
-		success:function(data){
-			return data;
-		}
-	});
-};
-
-var shuffleArray = function(array){
+	};
 	
-  var currentIndex = array.length,
-		temporaryValue,
-		randomIndex;
-
-  while (currentIndex !== 0) {
-
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-  
-  return array;
-};
-
-var setModals = function(){
-	$(".modal-wide").on("show.bs.modal", function() {
-		var height = $(window).height() - 200;
-		$(this).find(".modal-body").css("max-height", height);
-	});
-};
-
-
-var setPrepositionButtons = function(){
-	var btns = $("#btnsPrepositions").children(".btn");
-	btns.each(function(i, e){
-		var p = $(e).html(),
-			f = function(func, arg){
-				return (function(){
-					func(arg);
-				});
-			};
-		$(e).on('click', f(manager.toggleActivePrepositions,p));		
-	});
-};
-
-var setOptionButtons = function(){
-	var btns = $("#btnsNumberOptions").children(".btn");
-	btns.each(function(i, e){
-		var p = $(e).attr('name'),
-			f = function(func, arg){
-				return (function(){
-					func(arg);
-				});
-			};
-		$(e).on('click', f(manager.changeNumberOptions,p));		
-	});
-};
-
-var getRandomPrepositions = function(currentPreposition, numberNeeded){
-	var arr = manager.getActivePrepositions(),
-		i,
-		result = [];
-		
-	arr = shuffleArray(arr);
+	var setOptionNumber = function(){
+		$('#numberOptions').html('3');//should contain this variable within the manager module...
+	};
 	
-	for (i = 0; i < numberNeeded; i++){
-		if (arr[i] === currentPreposition){
-			numberNeeded++;
-			continue;
-		}
-		result.push(arr[i]);
-	}
-	return result;
+	setModals();
+	manager.setPrepositionButtons();
+	manager.setOptionButtons();
+	setOptionNumber();
+	$('#getSentence').on('click', manager.getNewSentence);
+	$('#checkAnswers').on('click', manager.checkAnswers);
+	$('#openModalLink').on('click', manager.linkAction);
+	manager.getNewSentence();
+	
 };
 
-var prepositionButtonAction = function(link){
-	return (function(){
-		var buttonName = $(this).html();
-		var linkName = link.html();
-		link.html(buttonName);
-		$(this).html(linkName);
-	});
-};
 
-var linkAction = function(){
-	var i,
-		m = $('#optionsButtons'),
-		action,
-		prepositions;
-	
-	currentLink = $(this);
-	currentLinkId = currentLink.attr('id');
-	//console.log('current link html: ' + currentLink.html());
-	m.children().remove();
-	prepositions = getRandomPrepositions(currentLink.html(), manager.getNumberOptions());
-	
-	for (i = 0; i < manager.getNumberOptions(); i++){
-		action = prepositionButtonAction(currentLink);
-		m.append($('<button>').attr({'class':'btn btn-default','type':'button','data-toggle':'modal','data-target':'#optionsModal'}).html(prepositions[i]).on('click', action));	
-	}
-};
 
 $(document).ready(function(){
-	setModals();
-	setPrepositionButtons();
-	setOptionButtons();
-	//buttonAction();
-	$('#openModalLink').on('click', linkAction);
+	
+	setup();
 
 });
